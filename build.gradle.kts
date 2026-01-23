ec6614dd60b41fcf8ddb7987dcf72b64052c4f3b
Victor Shpyrka <victor.shpyrka@gmail.com>
2026-01-23 16:38:38 +0200

    chore: Gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,6 @@ android {
 
     defaultConfig {
         minSdk = sdk.versions.minSdk.get().toInt()
-        targetSdk = sdk.versions.targetSdk.get().toInt()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -24,9 +23,6 @@ android {
                 "proguard-rules.pro"
             )
         }
-    }
-    kotlinOptions {
-        jvmTarget = "17"
     }
 
     buildFeatures {
